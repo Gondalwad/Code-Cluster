51223fe858e9200535a3598c44f9ff9032666e2b
gondalwad <sudarshangondalwad@gmail.com>
2025-09-04 23:42:29 +0530

    Upgreaded Auth service added global exception handler

--- AuthService/src/main/java/com/CodeCluster/AuthService/dto/LoginRequestDTO.java
+++ AuthService/src/main/java/com/CodeCluster/AuthService/dto/LoginRequestDTO.java
@@ -1,17 +1,21 @@
 package com.CodeCluster.AuthService.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class LoginRequestDTO {
 
     @NotBlank
-    @Size(min = 3)
+    @Size(min = 5, message = "Invalid username or Email")
     private String preferredId;
     @NotBlank
-    @Size(min = 6)
+    @Size(min = 8, message = "Length of password should be 8 or more")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must include uppercase, lowercase, digit, special character, and be at least 8 characters with no spaces"
+    )
     private String password;
-
 
     public String getPreferredId() {
         return preferredId;
